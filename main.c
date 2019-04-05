976287fb7c9d464d4dd0d249d89a9e3ef62b0af8
Daniel Sheedy <danmsheedy@gmail.com>
2019-04-05 13:41:30 +0000

    Setup Menu system. Had to learn & use sscanf and fgets to break-proof user interface.
    Setup switch flow control to choose which type of cipher the user wants to use.
    
    URGENT ISSUE: This program produces an infinite loop in Eclipse Che, but works fine on my own engine. - Why

--- main.c
+++ main.c
@@ -10,16 +10,46 @@ int main()
     printf("|    Welcome to Daniel Sheedy's Cyptographic Cipher Collection!    |\n");
     printf("|        ~ Submitted as an ENG1003 Programming Assignment ~        |\n\n");
     printf("                               MENU:                               \n\n");
-    printf("                 \u2023 0.   Rotation Cipher                        \n");
-    printf("                 \u2023 1.   Substitution Cipher                    \n");
+    printf("                 \u2023 1.   Rotation Cipher                        \n");
+    printf("                 \u2023 2.   Substitution Cipher                    \n");
+    int menuOption=0; char enterKey; char menuInput[200];
+	int continueMenuScreen=1;
+	Menu:
+	while(continueMenuScreen == 1) {
+		printf("\nEnter an option: ");
+		fgets(menuInput, sizeof(menuInput), stdin);
+		sscanf(menuInput,"%d%c", &menuOption, &enterKey);
+		if ((menuOption==1 || menuOption==2) && (enterKey = '\n')) {
+			continueMenuScreen = 0;
+		}
+		else {
+			printf("\n*** Warning: Please enter an integer 0 or 1***\n");
+			goto Menu;
+			break;
+		}
+	}
+		
+	switch(menuOption) {
+		case 1:
+			printf("\n\n --===ROTATION CIPHER===-- \n");
+			
+			char Input[200];
+			printf("Enter an input (UPPERCASE only): ");
+			scanf("%s", Input); //Seperate words with "_"
+//    		char Input[] = {65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90};
+            //ASCI A-Z Alphabet
+			int SizeC = strlen(Input); //sizeof(Input)/sizeof(Input[0]);
+			List(Input, SizeC);
+			break;
+			
+		case 2:
+			printf("\n\n --===SUBSTITUTION CIPHER===-- \n");
+			break;
+		default:
+            break;
+	}
+		
     
-    char Input[200];
-	printf("Enter an input (UPPERCASE only): ");
-	scanf("%s", Input); //Seperate words with "_"
-//    char Input[] = {65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90};
-                //ASCI A-Z Alphabet
-	int SizeC = strlen(Input); //sizeof(Input)/sizeof(Input[0]);
-    List(Input, SizeC);
     return 0;
 }
 
@@ -27,7 +57,7 @@ void List( char *a, int pn)
 {
     for(int x=0; x<pn; x++)
     {
-        printf("%c", *a+2); //         printf("%c", *a+KEY); Will Encrypt!!!
+        printf("%c", *a); //         printf("%c", *a+KEY); Will Encrypt!!!
         a++;
     }
 	printf("\n");
