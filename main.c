50ad01d68dd38890f65bc6d322ee42cb2a4ab0ed
Daniel Sheedy <danmsheedy@gmail.com>
2019-04-05 03:44:17 +0000

    Added basic menu layout

--- main.c
+++ main.c
@@ -4,13 +4,20 @@
 void List( char *a, char b);
 
 
-//Simple ASCI A-Z Alphabet
+
 int main()
 {
+    printf("|    Welcome to Daniel Sheedy's Cyptographic Cipher Collection!    |\n");
+    printf("|        ~ Submitted as an ENG1003 Programming Assignment ~        |\n\n");
+    printf("                               MENU:                               \n\n");
+    printf("                 \u2023 0.   Rotation Cipher                        \n");
+    printf("                 \u2023 1.   Substitution Cipher                    \n");
+    
+    
      char AlphabetASCII[] = {65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77,
-                      78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90};
-                      // A - Z
-     List(AlphabetASCII, 26);
+                                78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90};
+                            //ASCI A-Z Alphabet
+     List(AlphabetASCII, 0);
      return 0;
 }
 
