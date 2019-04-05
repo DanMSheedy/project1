996788104dd43875f7da1d25c0a712ca6a0e0ec1
Daniel Sheedy <danmsheedy@gmail.com>
2019-04-05 02:55:13 +0000

    Implemented simple array list function for the ASCII code alphabet.

--- main.c
+++ main.c
@@ -1,10 +1,22 @@
 #include <stdio.h>
-#include <math.h>
-
-int main() {
-
-    printf("Hello Demonstrator");
+void List( int *a, int b);
 
 
-return 0;
+//Simple ASCI A-Z Alphabet
+int main()
+{
+     int AlphabetASCII[] = {65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77,
+                      78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90};
+                      // A - Z
+     List(AlphabetASCII, 26);
+     return 0;
+}
+
+void List( int *a, int b)
+{
+    for(int x=0; x<b; x++)
+    {
+        printf("Value of index [%d] is: %d \n", x, *a);
+        a++;
+    }
 }
