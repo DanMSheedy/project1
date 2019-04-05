5e3e6d41cbb79400120c76b4cf71a14db6b5dd2f
Daniel Sheedy <danmsheedy@gmail.com>
2019-04-05 11:25:57 +0000

    Added scanf to take input. Text is now converted to ASCII and back to text and then printed. Discovered how to implement rotation cipher keys (see line 30).

--- main.c
+++ main.c
@@ -13,10 +13,12 @@ int main()
     printf("                 \u2023 0.   Rotation Cipher                        \n");
     printf("                 \u2023 1.   Substitution Cipher                    \n");
     
-    
-    char Input[] = {65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90};
+    char Input[200];
+	printf("Enter an input (UPPERCASE only): ");
+	scanf("%s", Input); //Seperate words with "_"
+//    char Input[] = {65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90};
                 //ASCI A-Z Alphabet
-	int SizeC = sizeof(Input)/sizeof(Input[0]);
+	int SizeC = strlen(Input); //sizeof(Input)/sizeof(Input[0]);
     List(Input, SizeC);
     return 0;
 }
@@ -25,7 +27,7 @@ void List( char *a, int pn)
 {
     for(int x=0; x<pn; x++)
     {
-        printf("%c", *a);
+        printf("%c", *a+2); //         printf("%c", *a+KEY); Will Encrypt!!!
         a++;
     }
 	printf("\n");
