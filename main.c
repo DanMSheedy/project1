fc9dddd926b943ef0ccd48468aab05bef13a18ca
Daniel Sheedy <danmsheedy@gmail.com>
2019-04-05 03:15:38 +0000

    Changed array element type from int to char.

--- main.c
+++ main.c
@@ -1,22 +1,24 @@
 #include <stdio.h>
-void List( int *a, int b);
+#include <math.h>
+
+void List( char *a, char b);
 
 
 //Simple ASCI A-Z Alphabet
 int main()
 {
-     int AlphabetASCII[] = {65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77,
+     char AlphabetASCII[] = {65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77,
                       78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90};
                       // A - Z
      List(AlphabetASCII, 26);
      return 0;
 }
 
-void List( int *a, int b)
+void List( char *a, char b)
 {
     for(int x=0; x<b; x++)
     {
-        printf("Value of index [%d] is: %d \n", x, *a);
+        printf("Value of index [%d] is: %c \n", x, *a);
         a++;
     }
 }
